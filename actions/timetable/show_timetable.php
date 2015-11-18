ebdb99d1fb767dfbe01c783d211a2f6b562ed514
pochta2id <pochta2id@gmail.com>
2015-11-18 15:51:54 +0700

    Change plus in comment <td> from text to background and several bug fixes.

--- actions/timetable/show_timetable.php
+++ actions/timetable/show_timetable.php
@@ -476,12 +476,12 @@ function create_timetable(){
 				for($dayFOR=1;$dayFOR<=$day_number;$dayFOR++){
 					$commentFOR=db_easy("SELECT * FROM `phpbb_timetable_comments` WHERE `year`=$Year AND `month`=$Month AND `day`=$dayFOR");
 					if($commentFOR['comment1']!='' || $commentFOR['comment2']!='' || $commentFOR['comment3']!=''){
-						$markFOR="+";
+						$styleFOR="background:url('/images/cross.png') center center no-repeat";
 					}else{
-						$markFOR="";
+						$styleFOR="";
 					}
 					$dayFOR==$day_number ? $td_g_class='glast' : $td_g_class='gnolast';
-					$html.="<td id='comment-$comment_number-$Year-$Month-$dayFOR' class='comment1' onDblClick=\"if(popup_id_check!=this.id) {popup_comment(this.id);}\" align='center'>$markFOR</td>";
+					$html.="<td id='comment-$comment_number-$Year-$Month-$dayFOR' class='comment1' style=\"$styleFOR\" onDblClick=\"if(popup_id_check!=this.id) {popup_comment(this.id);}\" align='center'></td>";
 				}
 				$html.="</tr>";
 			}
