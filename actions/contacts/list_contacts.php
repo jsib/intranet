7457bf8fcdd713d05dc0c86ac922872b66881226
root <root@web.adggroup.local>
2016-12-04 01:57:04 +0300

    Action edit_direction ready. Action list_contacts (now works with directions) works

--- actions/contacts/list_contacts.php
+++ actions/contacts/list_contacts.php
@@ -1,177 +1,226 @@
 <?php
-function list_contacts(){
-	//НАЧАЛО: Получаем направление сортировки
-	if(isset($_GET['sortdirection'])){
-		$sortdirection=$_GET['sortdirection'];
-	}else{
-		$sortdirection="asc";
-	}
-	if($sortdirection=="asc") $link_sortdirection="desc";
-	if($sortdirection=="desc") $link_sortdirection="asc";
-	//КОНЕЦ: Получаем направление сортировки
+function list_contacts(){ 
+	//Retrieve information from this function name
+	$function_name_pieces=explode("_", __FUNCTION__);
+
+	//Refer to global variables
+	global $table_prefix;
+	global $system_objects;
+
+	//Retrieve object properties
+	$object_plural_eng=$function_name_pieces[1];
+	$object_singular_eng=$system_objects[$object_plural_eng]['plural_name_eng'];
+	$object_actions=$system_objects[$object_plural_eng]['actions'];
 	
-	//НАЧАЛО: Получаем столбец для сортировки
+	//Retrieve action properties
+	$action_eng=$function_name_pieces[0];
+	$action_full_eng=__FUNCTION__;
+	
+	//Set parent object properties
+	$parent_object_singular_eng="direction";
+	$parent_object_plural_eng="directions";
+	
+	//Get sort direction
+	if(isset($_GET['sort_direction'])){
+		$sort_direction=$_GET['sort_direction'];
+	}else{
+		$sort_direction="asc";
+	}
+	
+	//Get links for sorting columns
+	if($sort_direction=="asc") $link_sort_direction="desc";  
+	if($sort_direction=="desc") $link_sort_direction="asc";
+	
+	//Get column for sorting
 	if(isset($_GET['sort'])){
 		$sort=$_GET['sort'];
 	}else{
-		$sort="username";
+		$sort="entity_name";
 	}
-	//КОНЕЦ: Получаем столбец для сортировки
 	
-	//НАЧАЛО: Получаем заголовки колонок для сортировки
-	$headers=array('username'=>array('rus'=>"ФИО", 'sortcolumn'=>"username"), 'branch'=>array('rus'=>'Подразделение', 'sortcolumn'=>"`phpbb_points`.`name`"));
+	//Setting table sorting
+	$headers=array( 'entity_name'=>array('rus'=>"ФИО", 'sort_column'=>"username"),
+					'parent_entity_name'=>array('rus'=>'Дирекция', 'sort_column'=>"`".$table_prefix.$parent_object_plural_eng."`.`name`"
+				   ));
+				   
+	//Build sorting links
 	foreach($headers as $name=>$value){
 		if($sort==$name){
-			$headers[$name]['html']="<a href='".uri_make(array('sortdirection'=>$link_sortdirection, 'sort'=>$name))."' class='header'>".$headers[$name]['rus']."<img src='/images/$sortdirection.png' class='header'></a>";
+			$headers[$name]['html']="<a href='".uri_make(array('sort_direction'=>$link_sort_direction, 'sort'=>$name))."' class='header'>".
+									$headers[$name]['rus']."<img src='/images/$sort_direction.png' class='header'></a>";
 		}else{
-			$headers[$name]['html']="<a href='".uri_make(array('sortdirection'=>'asc', 'sort'=>$name))."' class='header'>".$headers[$name]['rus']."</a>";
+			$headers[$name]['html']="<a href='".uri_make(array('sort_direction'=>'asc', 'sort'=>$name))."' class='header'>".$headers[$name]['rus']."</a>";
 		}
 	}
-	//КОНЕЦ: Получаем заголовки колонок для сортировки
 	
-	//НАЧАЛО: Строим фильтр городов
-	$branchesRES = db_query("SELECT * FROM `phpbb_branches` ORDER BY `name`");
+	//Build SQL expression for parent object filter
+	$parent_entities_db = db_query("SELECT * FROM `".$table_prefix.$parent_object_plural_eng."` ORDER BY 'name'");  
 
-	if(isset($_GET['branch']) && @$_GET['branch']!=1){
-		$branch_id=$_GET['branch'];
-		$branch_where1=" , `phpbb_branches`, `phpbb_points` ";
-		$branch_where2=" AND `phpbb_branches`.`id`=$branch_id ";
+	if(isset($_GET['parent_entity_id']) && @$_GET['parent_entity_id']!=1){
+		$parent_entity_id=$_GET['parent_entity_id'];
+		$parent_object_where=" AND `".$table_prefix.$parent_object_plural_eng."`.`id`=$parent_entity_id ";
 	}else{
-		$branch_where1="";
-		$branch_where2="";
+		$parent_object_where="";
 	}
 	
-	$branches_html="";
-	while($branch=db_fetch($branchesRES)){
-		if($branch_id==$branch['id']){
+	//Define HTML flow for list of parent entities
+	$parent_entities_html="";
+	while($parent_entityWHILE=db_fetch($parent_entities_db)){
+		if($parent_entity_id==$parent_entityWHILE['id']){
 			$selected="selected";
 		}else{
 			$selected="";
 		}
-		if($branch['id']!=1){
-			$branches_html.="<option value='{$branch['id']}' $selected>{$branch['name']}</option>";
+		
+		if($parent_entityWHILE['id']!=1){
+			$parent_entities_html.="<option value='".$parent_entityWHILE['id']."' $selected>".$parent_entityWHILE['name']."</option>";
 		}else{
-			$branches_html.="<option value='1' $selected>Все города</option>";
+			$parent_entities_html.="<option value='1' $selected>Все дирекции</option>";
 		}
 	}
 	
-	if(check_rights('show_hidden_contacts')){
-		if(@$_GET['show_hidden_contacts']=='on'){
-			$sql_hidden_contacts="OR `user_type`=9";
-			$hidden_contact_checked='checked';
+	//Form 'checked' flag for hidden entities
+	if(check_rights('show_hidden_entities')){
+		if(@$_GET['show_hidden_entities']=='on'){
+			$sql_hidden_entities="OR `user_type`=9";
+			$hidden_entity_checked='checked';
 		}else{
-			$sql_hidden_contacts="";
-		    $hidden_contact_checked='';
+			$sql_hidden_entities="";
+		    $hidden_entity_checked='';
 		}
 	}
-	//КОНЕЦ: Строим фильтр городов
 	
-	isset($_GET['branch']) ? $input_hidden_branch="<input type='hidden' name='branch' value='{$_GET['branch']}' />" : $input_hidden_branch="";
+	//Build hidden input for value of parent_entity_id. Need when you change select of parent_entities.
+	if(isset($_GET['parent_entity_id'])){
+		$parent_entity_id=$_GET['parent_entity_id'];
+		$input_hidden_entity="<input type='hidden' name='parent_entity_id' value='".$parent_entity_id."' />";
+	}else{
+		$input_hidden_entity="";
+	}
 	
-	if(check_rights('show_hidden_contacts')){
-		$filter_hidden_contacts=template_get("contacts/filter_hidden_contacts", array(
-														'input_hidden_branch'=>$input_hidden_branch,
-														'hidden_contact_checked'=>$hidden_contact_checked
+	//Build HTML for hidden entities checkbox
+	if(check_rights('show_hidden_'.$object_plural_eng)){
+		$checkbox_hidden_entities=template_get($object_plural_eng."/checkbox_hidden_entities", array(
+														'input_hidden_entity'=>$input_hidden_entity,
+														'hidden_entity_checked'=>$hidden_entity_checked
 												));
 	}
 	
-	$usersRES = db_query("SELECT *, `phpbb_branches`.`name` as `branch_name`, `phpbb_branches`.`id` as `branch_id`, 
-										`phpbb_points`.`phone` as `officephone`,
-										`phpbb_points`.`name` as `point_name`, `phpbb_points`.`id` as `point_id`
-									FROM `phpbb_users` , `phpbb_branches`, `phpbb_points` 
-									WHERE (`user_type` IN (0,3) $sql_hidden_contacts) AND `username`!='root' 
-											AND `phpbb_points`.`id`=`phpbb_users`.`point_id`
-											AND `phpbb_points`.`branch_id`=`phpbb_branches`.`id` 
-											$branch_where2
-									ORDER BY {$headers[$sort]['sortcolumn']} $sortdirection
-									
-									");
+	$entities_sql="SELECT *, `".$table_prefix.$parent_object_plural_eng."`.`name` as `parent_entity_name`, 
+										  `".$table_prefix.$parent_object_plural_eng."`.`id` as `parent_entity_id`
+									FROM  `".$table_prefix."users"."`, `".$table_prefix.$parent_object_plural_eng."`
+									WHERE (`user_type` IN (0,3) $sql_hidden_entities) AND `username`!='root' 
+											AND `".$table_prefix.$parent_object_plural_eng."`.`id`=`".$table_prefix."users"."`.".$parent_object_singular_eng."_id
+											$parent_object_where
+									ORDER BY {$headers[$sort]['sort_column']} ".$sort_direction;
+	$entities_db = db_query($entities_sql);
 	
-	$num_users=db_count($usersRES);
-	$num=0;
-	$table_html="";
-	if(check_rights('delete_contact')){
+	//echo $entities_sql;
+	
+	//Get number of entities retrieved from database
+	$entities_number=db_count($entities_db);
+	
+	//Define entities counter
+	$entities_counter=0;
+
+	//Add addition column for first row of table
+	if(check_rights('delete_'.$object_singular_eng)){
 		$th_html="		
 						<th class='right'></th>";
 	}else{
 		$th_html="";
 	}
-	while ($userWHILE = db_fetch($usersRES)){
-		$num++;
-		
-		$branch_points_number=db_easy_count("SELECT * FROM `phpbb_points` WHERE `branch_id`={$userWHILE['branch_id']}");
-		if($branch_points_number==1){
-			$branch_point_link="/manager.php?action=show_point&point=".$userWHILE['point_id'];
-		}else{
-			$branch_point_link="/manager.php?action=show_branch&branch=".$userWHILE['branch_id'];
-		}
-		
-		if($num==$num_users){
-			$bottom_class="bottom";
-		}else{
-			$bottom_class="";
-		}
-		
-		if(trim($userWHILE['officephone'])!=""){
-			$officephone=$userWHILE['officephone'];
-			if(trim($userWHILE['user_extphone'])!=""){
-				$officephone.=", доб. ".$userWHILE['user_extphone'];
-			}
-		}else{
-			$officephone="-";
-		}
-		
-		if(trim($userWHILE['user_workmobilephone'])!=""){
-			$mobilephone=$userWHILE['user_workmobilephone']." (рабочий)";
-		}elseif(trim($userWHILE['user_privatemobilephone'])!=""){
-			$mobilephone=$userWHILE['user_privatemobilephone']." (личный)";
-		}else{
-			$mobilephone="-";
-		}
-		
-		if(trim($userWHILE['user_occ'])==""){
-			$userWHILE['user_occ']="-";
-		}
-		
-		if(trim($userWHILE['point_id'])==1){
-			$userWHILE['point_name']="-";
-			$userWHILE['branch_name']="-";
-		}
-		
-		$userWHILE['user_type']==9 ? $style_hidden_contact='color:grey' : $style_hidden_contact='';
-		
-		if(check_rights('delete_contact')){$right_class='';}else{$right_class='right';}
-		$table_html.="	<tr class='$bottom_class'>
-							<td class='left'><a href='/manager.php?action=show_contact&contact=".$userWHILE['user_id']."' style='font-size:9pt;$style_hidden_contact'>".$userWHILE['username']."</a></td>
-							<td><a href='$branch_point_link' style='font-size:9pt;'>".$userWHILE['branch_name']."</a></td>
-							<td style='width:250px;'>".$userWHILE['user_occ']."</td>
-							<td style='width:250px;'>".$mobilephone."</td>
-							<td  class='$right_class'>".$officephone."</td>";
-		if(check_rights('delete_contact')){
-			$table_html.="	<td class='right'><a href='/manager.php?action=delete_contact&contact={$userWHILE['user_id']}' onclick=\"if(!confirm('Удалить?')) return false;\">Удалить</a><br/></td>
-						</tr>";
-		}
+	
+	//Get class for column before 'delete action' column
+	if(check_rights('delete_'.$object_singular_eng)){
+		$right_class='';
+	}else{
+		$right_class='right';
 	}
 	
-	//Ссылка "Добавить контакт"
-	if(check_rights('add_contact')){
-		$add_contact_html="<a href='/manager.php?action=add_contact' class='listcontacts'>Добавить сотрудника</a><br/><br/>";
+	//Define HTML flow for table
+	$table_html="";
+		
+	//Form table HTML
+	if($entities_number>0){
+		//First row
+		$table_html.="	<tr>
+							<th class='left'>".$headers['entity_name']['html']."</th>
+							<th>".$headers['parent_entity_name']['html']."</th>
+							<th>Должность</th>
+							<th>Мобильный</th>
+							<th class='".$right_class."'>Добавочный</th>".$th_html."</tr>";			
+
+		//Rows after first
+		while ($entity_while = db_fetch($entities_db)){
+			//Increase entity counter
+			$entities_counter++;
+					
+			if($entities_counter==$entities_number){
+				$bottom_class="bottom";
+			}else{
+				$bottom_class="";
+			}
+			
+			if(trim($entity_while['officephone'])!=""){
+				$officephone=$entity_while['officephone'];
+				if(trim($entity_while['user_extphone'])!=""){
+					$officephone.=", доб. ".$entity_while['user_extphone'];
+				}
+			}else{
+				$officephone="-";
+			}
+			
+			if(trim($entity_while['user_workmobilephone'])!=""){
+				$mobilephone=$entity_while['user_workmobilephone']." (рабочий)";
+			}elseif(trim($entity_while['user_privatemobilephone'])!=""){
+				$mobilephone=$entity_while['user_privatemobilephone']." (личный)";
+			}else{
+				$mobilephone="-";
+			}
+			
+			if(trim($entity_while['user_occ'])==""){
+				$entity_while['user_occ']="-";
+			}
+			
+			$entity_while['user_type']==9 ? $style_hidden_entity='color:grey' : $style_hidden_entity='';
+			
+			$parent_entity_link=uri_make(array('action'=>'show_'.$parent_object_singular_eng, 'entity_id'=>$entity_while[$parent_object_singular_eng."_id"]));
+			
+			$table_html.="	<tr class='$bottom_class'>
+								<td class='left'>".
+								"<a href='/manager.php?action=show_".$object_singular_eng."&entity_id=".$entity_while['user_id']."' style='font-size:9pt;$style_hidden_entity'>".
+								$entity_while['username']."</a></td>
+								<td><a href='".$parent_entity_link."' style='font-size:9pt;'>".$entity_while['parent_entity_name']."</a></td>
+								<td style='width:250px;'>".$entity_while['user_occ']."</td>
+								<td style='width:250px;'>".$mobilephone."</td>
+								<td  class='$right_class'>".$officephone."</td>";
+			
+			//'Delete' column
+			if(check_rights('delete_'.$object_singular_eng)){
+				$table_html.=
+					"	<td class='right'>".
+					"<a href='/manager.php?action=delete_".$object_singular_eng."&entity_id={$entity_while['user_id']}' onclick=\"if(!confirm('Удалить?')) return false;\">".
+					"Удалить</a><br/></td></tr>";
+			}
+		}
 	}else{
-		$add_contact_html="";
+		$table_html.="В данной дирекции нет сотрудников.";
+	}
+	
+	//Form add entity link
+	if(check_rights('add_'.$object_singular_eng)){
+		$add_entity_link="<a href='/manager.php?action=add_".$object_singular_eng."' class='listcontacts'>Добавить сотрудника</a><br/><br/>";
+	}else{
+		$add_entity_link="";
 	}
 
-	$html.=template_get("contacts/list_contacts", array(
-															'add_contact'=>$add_contact_html,
-															'numusers'=>$num_users,
+	return template_get($object_singular_eng."/list_".$object_plural_eng, array(
+															'add_entity_link'=>$add_entity_link,
+															'entities_number'=>$entities_number,
 															'table'=>$table_html,
-															'add_user'=>$add_user_html,
-															'branches'=>$branches_html,
-															'header[username]'=>$headers['username']['html'],
-															'header[branch]'=>$headers['branch']['html'],
-															'th_html'=>$th_html,
-															'right_class'=>$right_class,
-															'filter_hidden_contacts'=>$filter_hidden_contacts
-																));
-	return $html;
+															'parent_entities'=>$parent_entities_html,
+															'checkbox_hidden_entities'=>$checkbox_hidden_entities
+														));
 }
 ?>
--- actions/directions/edit_direction.php
+++ actions/directions/edit_direction.php
@@ -25,47 +25,79 @@ function edit_direction(){
 	$entity_id=$_GET['entity_id'];
 	
 	if(!isset($_POST['name'])){
+		//Form result message (if some presented)
+		if(isset($_GET['result'])){
+			//Retrive result from browser
+			$action_result=$_GET['result'];
+			
+			//Retrieve entity name from browser
+			$entity_name=@$_GET['name'];
+			
+			if(isset($object_actions[$action_eng]['results'][$action_result]['result'])){
+				//Retrieve action result message
+				$result_html=template_get("message", array('message'=>html_replace($object_actions[$action_eng]['results'][$action_result]['result'], array('name'=>$entity_name))));
+			}else{
+				dis_error("Result '".$action_result."' not defined with object '".$object_plural_eng."' and action '".$action_eng."'", 'echo', 'debug');
+			}
+		}
+		
 		$entity = db_easy("SELECT * FROM `".$table_prefix.$object_plural_eng."` WHERE `id`=$entity_id");
-		$show_entity_link="<a href='/manager.php?action=show_".$object_singular_eng."&".$object_singular_eng."_id=".$entity_id."&' style='font-size:8pt;'>Просмотреть</a>";
+		
+		//Form show entity link
+		$show_entity_link="<a href='/manager.php?action=show_".$object_singular_eng."&entity_id=".$entity_id."' style='font-size:8pt;'>Просмотреть</a>";
+		
+		//Form list entities link
+		$list_entities_link="<a href=\"/manager.php?action=list_".$object_plural_eng."\" style=\"font-size:8pt;color:black;text-decoration:underline;\">Все дирекции</a>";
 		
 		//Build HTML flow
-		$html=template_get("points/edit_point", array(		'action'=>"/manager.php?action=edit_point&point=$entity_id",
+		$html=template_get($object_plural_eng."/".$action_full_eng, array(
+																'list_entities_link'=>$list_entities_link,
+																'action'=>"/manager.php?action=".$action_full_eng."&entity_id=$entity_id",
 																'name'=>$entity['name'],
 																'address'=>$entity['address'],
 																'phone'=>$entity['phone'],
 																'branches'=>$branches_html,
 																'showpoint'=>$show_entity_link,
-																'message'=>$message_html
+																'message'=>$result_html
 																));
 	}else{
-		$entity['name']=trim($_POST['name']);
-		$entity['branch_id']=trim($_POST['branch']);
-		$do=true;
-		//Проверка на пустое название города
-		$entity['name']=trim($_POST['name']);
-		if(!preg_match("/^.{1,70}$/", $entity['name'])){
-			header("location: /manager.php?action=edit_point&point=$entity_id&message=emptypointname");
+		//Retrieve data from browser
+		$entity_name=trim($_POST['name']);
+		//echo "entity_id:".$entity_id;
+		//exit;
+
+		
+		//Define success checks flag
+		$do=true;		
+		
+		//Check for empty entity name
+		$entity_name=trim($_POST['name']);
+		if(!preg_match("/^.{1,70}$/", $entity_name)){
+			header("location: /manager.php?action=".$action_full_eng."&entity_id_id=".$entity_id."&result=empty_entity_name");
 			$do=false;
 		}
-		//Проверка на наличие города с таким же именем
-		$other_pointRES=db_query("SELECT * FROM `phpbb_points` WHERE `name`='{$entity['name']}'");
-		$other_point=db_fetch($other_pointRES);
-		if(db_count($other_pointRES)>0){
-			if($other_point['id']!=$entity_id){
-				header("location: /manager.php?action=edit_point&point=$entity_id&message=samepointexists");
+		
+		//Check for entity with same name
+		$other_entities_db=db_query("SELECT * FROM `".$table_prefix.$object_plural_eng."` WHERE `name`='".$entity_name."'");
+		$other_entity=db_fetch($other_entities_db);
+		if(db_count($other_entities_db)>0){
+			if($other_entity['id']!=$entity_id){
+				header("location: /manager.php?action=".$action_full_eng."&entity_id=".$entity_id."&result=same_entity_exists&name=".urlencode($entity_name));
 				$do=false;
 			}
 		}
+		
 		if($do){
-			db_query("UPDATE `phpbb_points`
-					SET `name`='{$entity['name']}',
-						`branch_id`={$entity['branch_id']},
-						`address`='{$entity['address']}',
-						`phone`='{$entity['phone']}'
-					WHERE `id`=$entity_id");
-			header("location: /manager.php?action=edit_point&point=$entity_id&message=pointsaved");
+			db_query("	UPDATE `".$table_prefix.$object_plural_eng."`
+						SET `name`='".$entity_name."'
+						WHERE `id`=".$entity_id);
+						
+			//Refer to edit entity page
+			header("location: /manager.php?action=".$action_full_eng."&entity_id=".$entity_id."&result=success");
 		}
 	}
+	
+	//Return HTML flow
 	return $html;
 }
 ?>
